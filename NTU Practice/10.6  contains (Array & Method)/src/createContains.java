00fe04e2019945563508b8ad84ef089dec7bb83d
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-01-08 21:28:41 +0200

    Tutorial

---
+++ NTU Practice/10.6  contains (Array & Method)/src/createContains.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Bastaff
+ *
+ */
+public class createContains {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
