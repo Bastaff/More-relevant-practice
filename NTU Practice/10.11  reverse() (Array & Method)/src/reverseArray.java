5d1ab01f98ed177adb2c7baee5e7df3c3b4b1dc8
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-01-09 19:42:14 +0200

    Complete

--- NTU Practice/10.11  reverse() (Array & Method)/src/reverseArray.java
+++ NTU Practice/10.11  reverse() (Array & Method)/src/reverseArray.java
@@ -1,11 +1,27 @@
+import java.util.Arrays;
+
 /**
  * @author Bastaff
  *
  */
 public class reverseArray {
 	public static void main(String[] args) {
+		int[] array = {1,2,3,4};
 
-
+		System.out.println(Arrays.toString(array));		 // Print array to check original state
+		reverse(array); 													// Reverse array after printing original
+		System.out.println(Arrays.toString(array)); 	// Check result after reversing
 	}
-
+	
+	public static void reverse(int[] array) {
+		int temp;
+		int fcount; // Forward count
+		int bcount; // Backwards count
+		
+		for (fcount = 0, bcount = array.length - 1; fcount < bcount ;++fcount, --bcount) {
+			temp = array[fcount];
+			array[fcount] = array[bcount];
+			array[bcount] = temp;
+		}
+	}
 }
