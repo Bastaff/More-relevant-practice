f2b796229e106ea75ade1e720d3ece3f33b53cdf
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-01-13 01:50:01 +0200

    Added Matrices - Unfinished

---
+++ NTU Practice/12.2 Matrices (2D Arrays)/src/Matrix.java
@@ -0,0 +1,37 @@
+
+public class Matrix {
+	public static void main(String[] args) {
+		int matrix[][] = {{1, 3, 5, 7}, {3, 4, 5, 6}, {8, 9, 3, 1}};
+		double matrixd[][] = {{1, 3, 5, 7}, {3, 4, 5, 6}, {8, 9, 3, 1}};
+		int matrix2[][] = {{6, 6, 6, 6}, {6, 6, 6, 6}, {6, 6, 6, 6}};
+		double matrixd2[][] = {{6, 6, 6, 6}, {6, 6, 6, 6}, {6, 6, 6, 6}};
+		print(matrix);
+		print(matrixd);
+		System.out.println(haveSameDimension(matrix, matrix2));
+		
+	}
+
+	public static void print(int[][] m) {
+		for (int[] row : m)
+			for (int x : row)
+				System.out.print(x + " ");
+	}
+
+	public static void print(double[][] m) {
+		for (double[] row : m)
+			for (double x : row)
+				System.out.print(x + " ");
+	}
+	
+	public static boolean haveSameDimension(int[][] m, int[][] m2) {
+		boolean haveSame = false;
+		for(int[] row : m) {
+			for (int[] row2 : m2) {
+				if(row == row2) {
+					haveSame = true;
+				}
+			}
+		}
+		return haveSame;
+	}
+}
