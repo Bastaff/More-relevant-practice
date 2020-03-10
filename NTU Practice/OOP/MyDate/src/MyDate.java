0392e8a9284ae3de9d260e5a9096c6fdd9304df6
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-03-10 23:15:33 +0200

    MyDate

--- NTU Practice/OOP/MyDate/src/MyDate.java
+++ NTU Practice/OOP/MyDate/src/MyDate.java
@@ -1,4 +1,40 @@
 
 public class MyDate {
+private int year;
+private int month;
+private int day;
+
+private String[] MONTHS = {"Jan", "Feb","Mar","Apr","May","Jun","Jul",
+							"Aug","Sep","Oct","Nov","Dec"};
+private String[] DAYS = {"Monday","Tuesday","Wednesday","Thursday",
+							"Friday","Saturday","Sunday"};
+private int[] DAYS_IN_MONTHS = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+	public boolean isLeapYear(int year) {
+		if (year % 4 != 0) {
+			return false;
+		} else if (year % 400 == 0) {
+			return true;
+		} else if (year % 100 == 0) {
+			return false;
+		} else
+		return true;
+	}
+	
+	public boolean isValidDate(int year, int month, int day) {
+			
+			if (year >= 1 && year <= 9999 ) {
+				return true;
+			}
+			if (month >= 1 && month <= 12) {
+				return true;
+			}
+			if (day >= 1 && day <= 31 )
+	}
+
+
+
+
+
 
 }
