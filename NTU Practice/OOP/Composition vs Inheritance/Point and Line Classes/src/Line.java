efd2a13f48f6f49bb7b2cc4c201e1a78b79e0718
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-03-18 17:47:38 +0200

    Finished Point and Line Classes

---
+++ NTU Practice/OOP/Composition vs Inheritance/Point and Line Classes/src/Line.java
@@ -0,0 +1,87 @@
+
+public class Line {
+private Point begin;
+private Point end;
+
+
+	public Line(Point begin, Point end) {
+		this.begin = begin;
+		this.end = end;
+	}
+	
+	public Line(int beginX, int beginY, int endX, int endY) {
+		begin = new Point(beginX, beginY);
+		end = new Point(endX, endY);
+	}
+	
+	public Point getBegin() {
+		return begin;
+	}
+	public Point getEnd() {
+		return end;
+	}
+	
+	public void setBegin(Point begin) {
+		this.begin = begin;
+	}
+	
+	public void setEnd(Point end) {
+		this.end = end;
+	}
+	
+	
+	
+	public int getBeginX() {
+		return begin.getX();
+	}
+	
+	public int getBeginY() {
+		return begin.getY();
+	}
+	
+	public int getEndX() {
+		return end.getX();
+	}
+	
+	public int getEndY() {
+		return end.getY();
+	}
+	
+	public void setBeginX(int beginX) {
+		begin.setX(beginX);
+	}
+	
+	public void setBeginY(int beginY) {
+		begin.setY(beginY);
+	}
+	
+	public void setBeginXY(int beginX, int beginY) {
+		begin.setXY(beginX, beginY);
+	}
+	
+	public void setEndX(int endX) {
+		end.setX(endX);
+	}
+	
+	public void setEndY(int endY) {
+		end.setY(endY);
+	}
+	
+	public void setEndXY(int endX, int endY) {
+		end.setXY(endX, endY);
+	}
+	
+	
+	public int getLength() {
+		return (int) Math.sqrt(begin.getX()*end.getX() + begin.getY()*end.getY());
+	}
+	
+	public double getGradient() {
+		return Math.atan2(begin.getX(), begin.getY());
+	}
+	
+	public String toString() {
+		return "Begin = " + getBegin() + "End = " + getEnd() + " Begin X,Y = " + getBeginX() + "," + getBeginY() + " End X,Y = " + getEndX() + "," + getEndY();
+	}
+
+}
