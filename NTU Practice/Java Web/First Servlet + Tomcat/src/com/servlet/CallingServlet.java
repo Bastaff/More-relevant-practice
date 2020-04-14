505846794f271f454de7a66df0bc41286228abeb
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-04-14 23:58:47 +0300

    Added Cookies

--- NTU Practice/Java Web/First Servlet + Tomcat/src/com/servlet/CallingServlet.java
+++ NTU Practice/Java Web/First Servlet + Tomcat/src/com/servlet/CallingServlet.java
@@ -5,15 +5,18 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-// Calling this servlet to return the square of values in Add.java
+
+@WebServlet("/sq")
 public class CallingServlet extends HttpServlet {
 
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
 		int k = 0;
